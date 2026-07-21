90c17d7a21ad54733dc182331e034e7bdadf48db
Connor B. <bieszcon29@stu.cedarburg.k12.wi.us>
2026-07-20 20:19:34 -0600

    Please be it
    Its almost done lets hope?

--- vite.config.ts
+++ vite.config.ts
@@ -9,19 +9,18 @@ export default defineConfig({
 		SvelteKitPWA({
 			registerType: 'autoUpdate',
 
-			scope: '/ScoutHub27/',
-			 
+			workbox: {
+				navigateFallback: '/ScoutHub27/index.html'
+			},
+
 			manifest: {
 				name: 'HexScouter BioCore',
 				short_name: 'BioCore',
+				scope: '/ScoutHub27/',
 				start_url: '/ScoutHub27/',
 				display: 'standalone',
 				background_color: '#000000',
 				theme_color: '#000000'
-			},
-
-			workbox: {
-				navigateFallback: '/ScoutHub27/index.html',
 			}
 		})
 	]
